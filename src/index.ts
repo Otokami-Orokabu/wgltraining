f9f7125aba66b6bfa0aa1cef5c949e1f990c4119
Otokami-Orokabu <otokami.lab@gmail.com>
2021-03-04 08:31:54 +0900

    コメントをちょっと追加

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-class helloworld{
-    say(): void {
-        console.log(`Hello World!`);
-    }
-}
+const main = () =>{
+    // #canvas 要素の取得
+    const canvas = document.querySelector("#webgl-canvas");
 
-let hw = new helloworld();
-hw.say();
+    if(!(canvas instanceof HTMLCanvasElement)){
+        throw new Error(`<canvas>要素がありません。`);
+    }
+
+    // WebGL レンダリングコンテキストの取得
+    const gl = canvas.getContext(`webgl`);
+    if(!gl) {
+        throw new Error(`WebGL の初期化に失敗しました。`)
+    }
+
+}
+window.onload = main;
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,20 +3,26 @@ import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const config: webpack.Configuration ={
+    // 動作モード(とりあえず開発)
     mode: `development`,
-    // エントリーポイント
+    // エントリーポイントとなるプログラム
     entry: "./src/index.ts",
     output: {
+        // javascriptをまとめたファイルのファイル名
         filename: `bundle.js`,
+        // ファイルの出力先を指定
         path: path.resolve(process.cwd()+ "/dist")
     },
     module: {
         rules: [
             {
+                // typescriptファイルを指定
                 test: /\.ts$/,
                 use: {
+                    // typescriptファイルをトランスコンパイルする
                     loader: "ts-loader",
                     options: {
+                        // コンパイル時はwebpack用のtsconfig.jsonを使う設定
                         configFile: "webpack.tsconfig.json"
                     },
                 },
@@ -29,6 +35,7 @@ const config: webpack.Configuration ={
         ],
     },
     plugins: [
+        // index.htmlもwebpackで扱うプラグイン設定
         new HtmlWebpackPlugin({
             template: "./src/index.html"
         })
