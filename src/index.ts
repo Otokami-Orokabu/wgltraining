06b84622d54b96a8766752d9bc2eb887e05a19e3
Otokami-Orokabu <otokami.lab@gmail.com>
2021-03-04 22:11:33 +0900

    ";" が抜けてる

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 const main = () =>{
+    console.log("hello world");
+
     // #canvas 要素の取得
     const canvas = document.querySelector("#webgl-canvas");
 
@@ -9,7 +11,7 @@ const main = () =>{
     // WebGL レンダリングコンテキストの取得
     const gl = canvas.getContext(`webgl`);
     if(!gl) {
-        throw new Error(`WebGL の初期化に失敗しました。`)
+        throw new Error(`WebGL の初期化に失敗しました。`);
     }
 
 }
