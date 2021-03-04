9b497ff353441032a04de750c9e698c8e3e688f8
Otokami-Orokabu <otokami.lab@gmail.com>
2021-03-04 22:59:40 +0900

    Webpack+typescript+jest環境確認なのでここまで

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
 const main = () =>{
+    // Hello,World
     console.log("hello world");
-
-    // #canvas 要素の取得
-    const canvas = document.querySelector("#webgl-canvas");
-
-    if(!(canvas instanceof HTMLCanvasElement)){
-        throw new Error(`<canvas>要素がありません。`);
-    }
-
-    // WebGL レンダリングコンテキストの取得
-    const gl = canvas.getContext(`webgl`);
-    if(!gl) {
-        throw new Error(`WebGL の初期化に失敗しました。`);
-    }
-
 }
 window.onload = main;
